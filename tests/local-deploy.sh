ce09990c9dc0c7adb3bd4c2ed669616a8a76d1dd
Jack Ivanov <e601809@gmail.com>
2018-05-29 17:39:59 +0300

    Tests

--- tests/local-deploy.sh
+++ tests/local-deploy.sh
@@ -6,7 +6,7 @@ DEPLOY_ARGS="{provider: local, server: $LXC_IP, ssh_user: ubuntu, endpoint: $LXC
 
 if [ "${LXC_NAME}" == "docker" ]
 then
-  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -e "DEPLOY_ARGS=${DEPLOY_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && bash -x algo -e \"${DEPLOY_ARGS}\" --skip-tags apparmor"
+  docker run -it -v $(pwd)/config.cfg:/algo/config.cfg -v ~/.ssh:/root/.ssh -e "DEPLOY_ARGS=${DEPLOY_ARGS}" travis/algo /bin/sh -c "chown -R 0:0 /root/.ssh && source env/bin/activate && ansible-playbook deploy.yml -e \"${DEPLOY_ARGS}\" --skip-tags apparmor"
 else
-  bash -x algo -e "${DEPLOY_ARGS}" --skip-tags apparmor
+  ansible-playbook deploy.yml -e "${DEPLOY_ARGS}" --skip-tags apparmor
 fi
